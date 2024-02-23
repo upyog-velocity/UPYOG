b5cae0be9d955839ecd4e6490d2b0933fbe869e1
Vardaan <v.vardaan@worldish.se>
2024-02-23 13:26:15 +0530

    Added a holding id specific API end point in order to generate OTP on default mobile number

--- core-services/user-otp/src/main/java/org/egov/web/contract/RequestInfo.java
+++ core-services/user-otp/src/main/java/org/egov/web/contract/RequestInfo.java
@@ -23,4 +23,76 @@ public class RequestInfo {
     private String msgId;
     private String requesterId;
     private String authToken;
+
+    public String getApiId() {
+        return apiId;
+    }
+
+    public void setApiId(String apiId) {
+        this.apiId = apiId;
+    }
+
+    public String getVer() {
+        return ver;
+    }
+
+    public void setVer(String ver) {
+        this.ver = ver;
+    }
+
+    public Date getTs() {
+        return ts;
+    }
+
+    public void setTs(Date ts) {
+        this.ts = ts;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getDid() {
+        return did;
+    }
+
+    public void setDid(String did) {
+        this.did = did;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(String msgId) {
+        this.msgId = msgId;
+    }
+
+    public String getRequesterId() {
+        return requesterId;
+    }
+
+    public void setRequesterId(String requesterId) {
+        this.requesterId = requesterId;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
 }
--- core-services/user-otp/src/main/java/org/egov/web/controller/OtpController.java
+++ core-services/user-otp/src/main/java/org/egov/web/controller/OtpController.java
@@ -2,8 +2,10 @@ package org.egov.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.egov.domain.service.OtpService;
+import org.egov.web.contract.Otp;
 import org.egov.web.contract.OtpRequest;
 import org.egov.web.contract.OtpResponse;
+import org.egov.web.contract.RequestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +27,24 @@ public class OtpController {
     @PostMapping("/v1/_send")
     @ResponseStatus(HttpStatus.CREATED)
     public OtpResponse sendOtp(@RequestBody OtpRequest otpRequest) {
+        otpService.sendOtp(otpRequest.toDomain());
+        return OtpResponse.builder().
+                responseInfo(null).successful(true).build();
+    }
+
+    @PostMapping("/holding/v1/_send")
+    @ResponseStatus(HttpStatus.CREATED)
+    public OtpResponse sendHoldingOtp(@RequestBody OtpRequest otpRequest) {
+        Otp otp = new Otp();
+        otp.setMobileNumber("9999999999");
+        otp.setTenantId("pg");
+        otp.setUserType("citizen");
+        otp.setType("login");
+        RequestInfo requestInfo = new RequestInfo();
+        requestInfo.setApiId("");
+        requestInfo.setMsgId("1708674014261|en_IN");
+        otpRequest.setOtp(otp);
+        otpRequest.setRequestInfo(requestInfo);
         otpService.sendOtp(otpRequest.toDomain());
         return OtpResponse.builder().
                 responseInfo(null).successful(true).build();
