75daa6b4d0466cb0111896217f8518d09fe27ee9
mmeraj1010 <121943867+mmeraj1010@users.noreply.github.com>
2024-07-03 23:08:18 +0530

    Update install-deps.sh revert the changes

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -4,7 +4,7 @@ BRANCH="$(git branch --show-current)"
 
 INTERNALS="micro-ui-internals"
 
-cd $INTERNALS && echo "installing packages" && yarn add "@egovernments/digit-ui-react-components@1.7.0-beta.2" -W && yarn install && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
+cd $INTERNALS && echo "installing packages" && yarn add "@egovernments/digit-ui-react-components@1.7.0-beta.2" && yarn install && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
 cd ..
 
 rm -rf node_modules
