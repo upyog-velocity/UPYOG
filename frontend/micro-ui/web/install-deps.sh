22bc2bd6687a8fa5a3c35e7e7eea1bf745d6b2b2
mmeraj1010 <121943867+mmeraj1010@users.noreply.github.com>
2024-07-04 15:13:47 +0530

    Update install-deps.sh update the code to add the egovernment package folder

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -4,7 +4,7 @@ BRANCH="$(git branch --show-current)"
 
 INTERNALS="micro-ui-internals"
 
-cd $INTERNALS && echo "installing packages" && yarn add "@egovernments/digit-ui-react-components@1.7.0-beta.2" && yarn install && rm -rf node_modules/@egovernments && cp -rpf @egovernments-updated-ui node_modules/ && mv node_modules/@egovernments-updated-ui node_modules/@egovernments && rm -rf @egovernments-updated-ui && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
+cd $INTERNALS && echo "installing packages" && yarn add "@egovernments/digit-ui-react-components@1.7.0-beta.2" && yarn install && rm -rf node_modules/@egovernments && cp -rpf @egovernments-updated-ui node_modules/ && mv node_modules/@egovernments-updated-ui node_modules/@egovernments && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
 cd ..
 
 rm -rf node_modules
