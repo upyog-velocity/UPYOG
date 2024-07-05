7d3e0abed1f8d39e821746e7407fd2b32ebdff2f
mmeraj1010 <121943867+mmeraj1010@users.noreply.github.com>
2024-07-05 16:38:39 +0530

    Update install-deps.sh add the node module command

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -4,7 +4,7 @@ BRANCH="$(git branch --show-current)"
 
 INTERNALS="micro-ui-internals"
 
-cd $INTERNALS && echo "installing packages" && yarn install && echo "Move evog module file" && rm -rf node_modules/@egovernments && cp -rpf @egovernments-updated-ui node_modules/ && mv node_modules/@egovernments-updated-ui node_modules/@egovernments && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
+cd $INTERNALS && echo "installing packages" && yarn install && echo "Move evog module file" && rm -rf node_modules/@egovernments && cp -rpf @egovernments-new-updated-ui node_modules/ && mv node_modules/@egovernments-new-updated-ui node_modules/@egovernments && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
 cd ..
 echo "Delete web Node Module folder"
 rm -rf node_modules
