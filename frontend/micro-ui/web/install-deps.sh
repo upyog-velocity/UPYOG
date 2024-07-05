561da76bbaa7249e062c4bd515472b164767a806
mmeraj1010 <121943867+mmeraj1010@users.noreply.github.com>
2024-07-05 12:03:55 +0530

    Update install-deps.sh add the comments in this file

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -6,7 +6,7 @@ INTERNALS="micro-ui-internals"
 
 cd $INTERNALS && echo "installing packages" && yarn install && rm -rf node_modules/@egovernments && cp -rpf @egovernments-updated-ui node_modules/ && mv node_modules/@egovernments-updated-ui node_modules/@egovernments && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
 cd ..
-
+echo "Delete web Node Module folder"
 rm -rf node_modules
 rm -f yarn.lock
 
